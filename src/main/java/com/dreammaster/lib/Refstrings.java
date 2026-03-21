87d301147d73f6fc82a66e00500f06b421f473fe
akirapriva <eymencagan88@gmail.com>
2026-03-21 06:43:19 +0300

    rename

--- src/main/java/com/dreammaster/lib/Refstrings.java
+++ src/main/java/com/dreammaster/lib/Refstrings.java
@@ -3,15 +3,15 @@ package com.dreammaster.lib;
 public final class Refstrings {
 
     public static final String MODID = "dreamcraft";
-    public static final String NAME = "GT5 Unbound";
+    public static final String NAME = "GT5u Unbound";
     public static final String VERSION = BuildVersion.VERSION;
-    public static final String MODPACKPACK_VERSION = "1.0b";
+    public static final String MODPACKPACK_VERSION = "1.2a";
     public static final String CLIENTSIDE = "com.dreammaster.main.ClientProxy";
     public static final String SERVERSIDE = "com.dreammaster.main.CommonProxy";
     public static final String COLLECTIONID = "GT5U";
 
     public static final String WIKI_LINK = "https://wiki.gtnewhorizons.com/wiki/";
-    public static final String ISSUE_TRACKER_LINK = "https://github.com/akirapriva/GT5U-Core";
+    public static final String ISSUE_TRACKER_LINK = "https://github.com/akirapriva/GT5UU-Core";
     public static final String DISCORD_LINK = "https://discord.gg/WM4uJ6kdbF";
 
     private Refstrings() {}
